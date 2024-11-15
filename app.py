494228217ce71cd746c922c6d3d11b53356187f9
allen smth <141290976+allensmth@users.noreply.github.com>
2024-11-15 01:59:50 +0000

    socket

--- app.py
+++ app.py
@@ -1,60 +1,17 @@
-from flask import Flask, render_template, request
-from flask_assets import Environment, Bundle
-
-
-app = Flask(__name__)
-
-assets = Environment(app)
-css = Bundle("src/main.css", output="dist/main.css")
-js = Bundle("src/*.js", output="dist/main.js")
-
-assets.register("css", css)
-assets.register("js", js)
-
-css.build()
-js.build()
-
-
-class User:
-    id = 0
-    def __init__(self, fname, lname, email ):
-        User.id  += 1
-        self.id = User.id 
-        self.fname = fname
-        self.lname = lname 
-        self.email = email
-
-    def search( self, word ):
-        if (word is None):
-            return False
-        all = self.fname + self.lname + self.email
-        return word.lower() in all.lower()
-
-users = [
-    User("abe", "vida", "abe@nowhere.com"),
-    User("betty", "b", "bb@nowhere.com"),
-    User("joe", "robinson", "jrobinson@nowhere.com"),
-
-    User("Luis", "Cortes", "Luis@somewhere.com"),
-    User("marty", "hinkle", "mhinkle@nowhere.com"),
-    User("matthew", "robinson", "mrobinson@nowhere.com"),
-
-    User("collin", "western", "cwest@nowhere.com"),
-    User("marty", "hinkle II", "mhinkle2@nowhere.com"),
-    User("joe", "robinson", "jrobinson@nowhere.com"),
-
-    User("juan", "vida", "juanvida@nowhere.com"),
-    User("marty", "hinkle III", "mhinkle3@nowhere.com"),
-    User("zoe", "omega", "zoe@nowhere.com") 
-]
-
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+# 加载环境变量
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+socketio = SocketIO(app, async_mode='eventlet')
 
 class RSS(db.Model):
     __tablename__ = 'rss'
@@ -62,20 +19,53 @@ class RSS(db.Model):
     content = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     label = db.Column(db.String, nullable=True)
-    created_at = db.Column(db.Time, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
 
 @app.route('/')
 def index():
     rss_items = RSS.query.all()
     return render_template('index.html', rss_items=rss_items)
 
-@app.route("/search", methods=["POST"])
-def search():
-    word = request.form.get("search")
-    if (word is None or word == ""):
-        return render_template("search.html", users=[])
-    else:
-        return render_template("search.html", users=filter(lambda u: u.search(word), users))
-    
+@app.route('/add_rss', methods=['POST'])
+def add_rss():
+    data = request.get_json()
+    content = data.get('content')
+    author = data.get('author')
+    label = data.get('label')
+    created_at = data.get('created_at', datetime.utcnow())
+
+    if not content or not author:
+        return jsonify({'error': 'Content and author are required'}), 400
+
+    new_rss = RSS(content=content, author=author, label=label, created_at=created_at)
+    db.session.add(new_rss)
+    db.session.commit()
+
+    # 通过 WebSocket 通知客户端
+    socketio.emit('new_rss', {
+        'id': new_rss.id,
+        'content': new_rss.content,
+        'author': new_rss.author,
+        'label': new_rss.label,
+        'created_at': new_rss.created_at.strftime('%Y-%m-%d %H:%M:%S')
+    })
+
+    return jsonify({'message': 'RSS item added successfully'}), 201
+
+@app.route('/search_rss', methods=['GET'])
+def search_rss():
+    label = request.args.get('label')
+    if not label:
+        return jsonify({'error': 'Label is required'}), 400
+
+    rss_items = RSS.query.filter_by(label=label).all()
+    return jsonify([{
+        'id': item.id,
+        'content': item.content,
+        'author': item.author,
+        'label': item.label,
+        'created_at': item.created_at.strftime('%Y-%m-%d %H:%M:%S')
+    } for item in rss_items])
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
